bcc0b324ae12ee97b8f832b1aa10f733951d3217
Kai Arnold <cdn_kai@yahoo.com>
2015-12-30 15:42:19 -0800

    first set of extractions

--- isaacItemTableExtract.js
+++ isaacItemTableExtract.js
@@ -1,14 +1,26 @@
-// Current workflow is to paste this right into the dev console with the Isaac wiki page open, and
-// run it manually
+// Current workflow:
 //
-// i.e. extractToJSON(getTable(0)).
-//
-function extractToJSON(table)
+// 1) open the isaac rebirth wiki page to scrape
+// 2) paste this file directly in the dev console
+// 3) run scrapeTable() and scrapeTableSublementalTemplate, save them directly to files
+// 3.5) there may be more than one table of items to scrape per page, so pass in different table indices as appropriate
+// 4) save the files where the combiner script can find them
+
+var g_scrapeDescriptionColumn = 2; // == 3 for cards and runes
+function scrapeTable(tableIndex)
+{
+	return JSON.stringify(extractToJSON(getTable(tableIndex), rowImageAndThumbnailScrape), null, '\t');
+}
+function scrapeTableSublementalTemplate(tableIndex)
+{
+	return JSON.stringify(extractToJSON(getTable(tableIndex), rowMetadataTemplateScrape), null, '\t');
+}
+function extractToJSON(table, rowScraper)
 {
 	var retval = {};
 
 	var nRows = table.rows.length;
-	for (var r = 1; r < 5; ++r)
+	for (var r = 1; r < nRows; ++r)
 	{
 		var row = table.rows[r];
 		var nCols = row.cells.length;
@@ -17,18 +29,30 @@ function extractToJSON(table)
 
 		var nameAnchor  = row.cells[0].childNodes[0];
 		var name = nameAnchor.childNodes[0].textContent;
-		entry.wikiPage = nameAnchor.href;
 
-		var thumbnail = row.cells[1].childNodes[0].childNodes[0];
-		entry.thumbnail = thumbnail.src;
+		rowScraper(row, entry);
 
-		var description = row.cells[2];
-		entry.descriptionHTML = description.innerHTML;
 		retval[name] = entry;
 	}
 	return retval;
 }
+function rowImageAndThumbnailScrape(row, entry)
+{
+	entry.wikiPage = row.cells[0].childNodes[0].href;
 
+	var thumbnail = row.cells[1].childNodes[0].childNodes[0];
+	entry.thumbnail = thumbnail.src;
+
+	var description = row.cells[g_scrapeDescriptionColumn];
+	entry.descriptionHTML = description.innerHTML;
+}
+function rowMetadataTemplateScrape(row, entry)
+{
+	entry.itemClass = "";
+	entry.itemType = "";
+	entry.itemColor = "";
+	entry.itemTags = "";
+}
 function getTable(index)
 {
 	return document.body.getElementsByTagName("table")[index];
